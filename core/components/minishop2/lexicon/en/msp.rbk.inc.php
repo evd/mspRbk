6fdecd7f076829794e596413937025809c67ab3c
evd <toevgd@gmail.com>
2013-06-15 16:49:26 +0400

    Update translation for settings

--- core/components/minishop2/lexicon/en/msp.rbk.inc.php
+++ core/components/minishop2/lexicon/en/msp.rbk.inc.php
@@ -7,15 +7,22 @@
  */
 
 $_lang['ms2_payment_rbk_service_name'] = 'Order #[[+num]]';
-$_lang['setting_ms2_payment_rbk_checkout_url'] = 'RBKMoney checkout url';
-$_lang['setting_ms2_payment_rbk_currency'] = 'RBKMoney currency';
-$_lang['setting_ms2_payment_rbk_eshopid'] = 'RBKMoney eshopId';
-$_lang['setting_ms2_payment_rbk_secret_key'] = 'RBKMoney secret_key';
-$_lang['setting_ms2_payment_rbk_language'] = 'RBKMoney language';
-$_lang['setting_ms2_payment_rbk_language_desc'] = 'Only ru for russian or en for english';
-$_lang['setting_ms2_payment_rbk_preference'] = 'RBKMoney preference payment';
-$_lang['setting_ms2_payment_rbk_preference_desc'] = 'bankcard for creditcart, postrus for post etc. Or empty for user select';
-$_lang['setting_ms2_payment_rbk_success_id'] = 'RBKMoney successful page id';
-$_lang['setting_ms2_payment_rbk_cancel_id'] = 'RBKMoney cancel page id';
-$_lang['setting_ms2_payment_rbk_cancel_order'] = 'RBKMoney cancel order';
+
+$_lang['setting_ms2_payment_rbk_checkout_url'] = 'RBK Money checkout url';
+$_lang['setting_ms2_payment_rbk_checkout_url_desc'] = 'Url to request payment in RBK Money.';
+$_lang['setting_ms2_payment_rbk_currency'] = 'RBK Money currency';
+$_lang['setting_ms2_payment_rbk_currency_desc'] = 'Payment\'s currecnt in RBK Money. Available values: USD, RUR, EUR, UAH, GBP.';
+$_lang['setting_ms2_payment_rbk_eshopid'] = 'RBK Money eshopId';
+$_lang['setting_ms2_payment_rbk_eshopid_desc'] = 'Unique shop\'s identifier. You can see it in the personal cabinet in RBK Money.';
+$_lang['setting_ms2_payment_rbk_secret_key'] = 'RBK Money secret_key';
+$_lang['setting_ms2_payment_rbk_secret_key_desc'] = 'Mandatory secret key to confirm all transactions. You can set it in the personal cabinet in RBk Money.';
+$_lang['setting_ms2_payment_rbk_language'] = 'RBK Money language';
+$_lang['setting_ms2_payment_rbk_language_desc'] = 'User interface language. Available values: ru, en.';
+$_lang['setting_ms2_payment_rbk_preference'] = 'RBK Money preference payment';
+$_lang['setting_ms2_payment_rbk_preference_desc'] = 'Preffered input method for payment bypassing selection page. More in RBK Money\'s documenation. Or empty for user select.';
+$_lang['setting_ms2_payment_rbk_success_id'] = 'RBK Money successful page id';
+$_lang['setting_ms2_payment_rbk_success_id_desc'] = 'The customer will be sent to this page after the completion of the payment. It is recommended to specify the id of the page with the shopping cart to order output.';
+$_lang['setting_ms2_payment_rbk_cancel_id'] = 'RBK Money cancel page id';
+$_lang['setting_ms2_payment_rbk_cancel_id_desc'] = 'The customer will be sent to this page if something went wrong. It is recommended to specify the id of the page with the shopping cart to order output.';
+$_lang['setting_ms2_payment_rbk_cancel_order'] = 'RBK Money cancel order';
 $_lang['setting_ms2_payment_rbk_cancel_order_desc'] = 'If true, order will be cancelled if customer cancel payment.';
--- core/components/minishop2/lexicon/ru/msp.rbk.inc.php
+++ core/components/minishop2/lexicon/ru/msp.rbk.inc.php
@@ -7,16 +7,22 @@
  */
 
 $_lang['ms2_payment_rbk_service_name'] = 'Заказ #[[+num]]';
+
 $_lang['setting_ms2_payment_rbk_checkout_url'] = 'Url оплаты RBK Money';
-$_lang['setting_ms2_payment_rbk_currency'] = 'Валюта на RBK Money';
-$_lang['setting_ms2_payment_rbk_currency_desc'] = 'USD, RUR, EUR, UAH, GBP';
+$_lang['setting_ms2_payment_rbk_checkout_url_desc'] = 'Url для отправки запроса на оплату в системе RBK Money.';
+$_lang['setting_ms2_payment_rbk_currency'] = 'Валюта RBK Money';
+$_lang['setting_ms2_payment_rbk_currency_desc'] = 'Валюта в которой производится оплата в системе RBK Money. Возможные значения: USD, RUR, EUR, UAH, GBP.';
 $_lang['setting_ms2_payment_rbk_eshopid'] = 'Идентификатор магазина в RBK Money';
-$_lang['setting_ms2_payment_rbk_secret_key'] = 'Секретрый ключ RBK Money';
+$_lang['setting_ms2_payment_rbk_eshopid_desc'] = 'Обязательный уникальный номер магазина в системе RBK Money. Узнать его можно в личном кабинете RBK Money.';
+$_lang['setting_ms2_payment_rbk_secret_key'] = 'Секретный ключ RBK Money';
+$_lang['setting_ms2_payment_rbk_secret_key_desc'] = 'Обязательный секретный ключ для подтверждения всех транзакций. Его можно указать в личном кабинете RBK money.';
 $_lang['setting_ms2_payment_rbk_language'] = 'Язык интерфейса RBK Money';
-$_lang['setting_ms2_payment_rbk_language_desc'] = 'ru или en';
+$_lang['setting_ms2_payment_rbk_language_desc'] = 'Язык интерфейса в системе RBK Money. Возможные варианты ru или en.';
 $_lang['setting_ms2_payment_rbk_preference'] = 'Способ пополнения в RBK Money';
-$_lang['setting_ms2_payment_rbk_preference_desc'] = 'bankcard для кредитных карт, postrus для почты России итд. Или пустое для выбора пользователем';
+$_lang['setting_ms2_payment_rbk_preference_desc'] = 'Способ ввода средств в систему RBK Money, который будет предложен Плательщику, минуя экран выбора. Подробнее в документации к RBK Money. Или пустое для выбора пользователем.';
 $_lang['setting_ms2_payment_rbk_success_id'] = 'Страница успешной оплаты RBK Money';
+$_lang['setting_ms2_payment_rbk_success_id_desc'] = 'Пользователь будет отправлен на эту страницу после завершения оплаты. Рекомендуется указать id страницы с корзиной, для вывода заказа.';
 $_lang['setting_ms2_payment_rbk_cancel_id'] = 'Страница отказа от оплаты RBK Money';
+$_lang['setting_ms2_payment_rbk_cancel_id_desc'] = 'Пользователь будет отправлен на эту страницу при неудачной оплате. Рекомендуется указать id страницы с корзиной, для вывода заказа.';
 $_lang['setting_ms2_payment_rbk_cancel_order'] = 'Отмена заказа RBK Money';
-$_lang['setting_ms2_payment_rbk_cancel_order_desc'] = 'Если включено, заказ будет отменён при отказе от оплааты.';
+$_lang['setting_ms2_payment_rbk_cancel_order_desc'] = 'Если включено, то заказ будет перевед в статус "Отменён" при отказе от оплаты.';
